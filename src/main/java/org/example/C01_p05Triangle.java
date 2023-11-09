16b71efa82c622974d06355e8409e91823ea8930
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-09 21:37:46 +0000

    Update C01_p05Triangle.java

--- src/main/java/org/example/C01_p05Triangle.java
+++ src/main/java/org/example/C01_p05Triangle.java
@@ -5,12 +5,9 @@ class triangle{
     public static void main (String[] args){
         int height = 7;
         int width = 5;
-        int area;
-        int perimeter;
-        area = height* width;
+        double  area;
+        area = height* width/2;
         System.out.println("Triangle area is "+ area+" square inches");
-        perimeter = (height+width)*2;
-        System.out.println("Triangle perimeter is "+ perimeter+" inches");
 
         }
 
