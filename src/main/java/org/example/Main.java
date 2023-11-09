b199b86aefc4d0aa5d23fb36715e4a145a3661b0
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-09 20:54:29 +0000

    Area and Perimeter of Rectangle

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+class C01_P05{
+
+    public static void main (String[] args){
+        int height = 7;
+        int width = 5;
+        int area;
+        int perimeter;
+        area = height* width;
+        System.out.println("Triangle area is "+ area+" inches");
+        perimeter = (height+width)*2;
+        System.out.println("Triangle perimeter is "+ perimeter+" inches");
+
+        }
+
+
+
+        }
