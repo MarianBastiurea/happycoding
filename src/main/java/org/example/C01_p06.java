e958dca133815888290e3d62a0edf73f785332f9
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-09 21:19:12 +0000

    Area and Perimeter of a Circle

---
+++ src/main/java/org/example/C01_p06.java
@@ -0,0 +1,14 @@
+package org.example;
+
+class Circle {
+    public static void main(String[] args) {
+        double area;
+        double perimeter;
+        int radius=6;
+        perimeter = 2* 3.14 * radius;
+        area = 3.14*radius*radius;
+        System.out.println("Perimeter of the Circle is "+ perimeter+" inches");
+        System.out.println("Area of the Circle is "+area+" square inches");
+
+        }
+    }
