95fc1510c2da7269472653dff1d6264f981b68a2
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-10 21:17:46 +0000

    Convert days in years, weeks and days

---
+++ src/main/java/org/example/C01_p08.java
@@ -0,0 +1,20 @@
+package org.example;
+
+class C01_p08 {
+    public static void main(String[] args) {
+        int years;
+        int weeks;
+        int days;
+        int a=1329;
+        int a1;
+        int a2;
+        int a3;
+
+        a1=(int) a/365;
+        a2= (a-a1*365)/7;
+        a3=a-a1*365-a2*7;
+
+    System.out.println("If convert 1329 days in years, weeks and days, result will be= "+a1+" years, "+a2+" weeks and "+a3+" days");
+
+    }
+}
