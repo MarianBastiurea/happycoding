6006c58cba3d1b8224c61f2fee133dd4ec3b00e6
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-10 21:18:33 +0000

    Sum of two integer from the users

---
+++ src/main/java/org/example/C01_p09.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import com.sun.source.util.DocTreeScanner;
+
+import java.util.Scanner;
+
+class C01_p09 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        int d;
+        int a2;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first integer:");
+        a=sc.nextInt();
+        System.out.println("Enter second integer: ");
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("Sum is "+c);
+
+
+        }
+
+
+
+}
