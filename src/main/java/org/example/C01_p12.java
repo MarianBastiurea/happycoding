bd773eb10d97e1bd0055e24f72d00af78f7ca795
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-11 20:14:58 +0000

    Create C01_p12.java
    Print employee ID and monthly salary

---
+++ src/main/java/org/example/C01_p12.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+class C01_p12{
+    public static void main(String[] args) {
+        String id="0342";
+        double hourlySalary;
+        double workedHours;
+        double monthlySalary;
+        String a;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter employee ID:");
+        a=sc.next();
+        System.out.println("Enter employee hourly salary: ");
+        hourlySalary=sc.nextDouble();
+        System.out.println(" Enter employee worked hours; ");
+        workedHours= sc.nextDouble();
+        monthlySalary=hourlySalary*workedHours;
+        System.out.printf( "Employee Id: "+id+" and monthly salary is: %.2f", monthlySalary);
+        }
+    }
