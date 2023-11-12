aaa6873c2d017240d82336a12fbbd516f1de73d7
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-12 17:43:34 +0000

    check if two numbers are multiplied

---
+++ src/main/java/org/example/C01_p24.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+class C01_p24 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first integer:");
+        a = sc.nextInt();
+        System.out.println("Enter second integer: ");
+        b = sc.nextInt();
+        if (b > a) {
+            if (b % a == 0) {
+                System.out.println("Number are multiplied");
+            } else {
+                System.out.println("Number aren't multiplied");
+            }
+        }
+        }
+
+}
+
