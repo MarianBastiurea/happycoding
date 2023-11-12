ac0a6b779a1d00c68c7c16b395e006c3d3ff88a0
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-12 17:58:53 +0000

    Count positive and negative number
    count positive and negative numbers given by users

---
+++ src/main/java/org/example/C01_p27.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+class C01_p27 {
+    public static void main(String[] args) {
+        int a=0;
+        int b=0;
+        int[] myArray=new int[5];
+        Scanner sc=new Scanner(System.in);
+        for (int i=0; i<5; i++) {
+            System.out.println("Enter number:");
+            myArray[i]=sc.nextInt();
+        }
+        for ( int i=0; i<5; i++){
+        if (myArray[i]<0) {
+            a=a+1;}
+        else {
+            b=b+1;
+        }
+        }
+        System.out.println("there are: "+b+" positive numbers");
+        System.out.println("there are: "+a+" negative numbers");
+        }
+    }
