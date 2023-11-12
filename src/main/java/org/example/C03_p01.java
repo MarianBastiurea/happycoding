5af562f0ef397624727abe8fb5fb20d9eb05f984
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-12 19:12:38 +0000

    Print firts 10 integers

---
+++ src/main/java/org/example/C03_p01.java
@@ -0,0 +1,12 @@
+package org.example;
+
+class C03_p01 {
+    public static void main(String[] args) {
+        int a=0;
+        for (int i=0; i<10; ++i) {
+            a = a + 1;
+            System.out.print(a);
+        }
+        }
+
+}
