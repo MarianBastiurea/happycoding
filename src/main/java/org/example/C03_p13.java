46776d8fbe40706d7a86a73a4b3be104495b5e7f
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-14 20:37:27 +0000

    Display a pyramid pattern with numbers increase by 1

---
+++ src/main/java/org/example/C03_p13.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+class C03_p13 {
+    int row;
+    public static void main(String[] args) {
+        int a;
+        int b=0;
+        int row;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Integer: ");
+        a = sc.nextInt();
+        int rows = a - 1;
+        for (int i = 1; i <= a; ++i) {
+            for (int j = rows; j > 0; j--) {
+                System.out.print(" ");
+            }
+            rows = rows - 1;
+            for (int c = 1; c <= i; ++c) {
+                b=b+1;
+                System.out.print(b+" ");
+            }
+                System.out.println("");
+        }
+    }
+}
