b81cd6fe5afd7eab0104b284419f7fdfa5d7c509
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-17 14:53:59 +0000

    Display Floyd's triangle

---
+++ src/main/java/org/example/C03_p22.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+class C03_p19 {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        a=sc.nextInt();
+        for (int i=1; i<=a; ++i) {
+            if (i % 2 == 0) {
+                for (int j = 1; j <= i; ++j) {
+                    if (j %2==0){
+                        System.out.print("1 ");
+                    }
+                    else {
+                        System.out.print("0 ");
+                    }
+                }
+            }
+            else {
+                for (int j = 1; j <= i; ++j) {
+                    if (j % 2 == 0) {
+                        System.out.print("0 ");
+                    } else {
+                        System.out.print("1 ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
