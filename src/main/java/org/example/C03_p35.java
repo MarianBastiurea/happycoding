9a39b67b475f2e4337c8f30a53a1c6a645b5cf98
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-19 16:12:13 +0000

    Display first n terms of Fibonacci

---
+++ src/main/java/org/example/C03_p35.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+class C03_p35 {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        a= sc.nextInt();
+        int[] myArray= new int[a];
+        int b=0;
+        int c=1;
+        System.out.print( b+" "+c+" ");
+        for (int i=2; i<a;++i) {
+            int sum =b+c;
+                System.out.print(sum+" ");
+                b=c;
+                c=sum;
+        }
+    }
+}
