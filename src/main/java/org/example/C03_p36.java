6dc9824c64cd42a1a49ef7636aa3813c5fd68ae2
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-19 16:56:50 +0000

    Display a pattern like a triangle
    Display a pattern like a triangle with n rows where first and last nuber is 1

---
+++ src/main/java/org/example/C03_p36.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+ class C03_p36 {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        a= sc.nextInt();
+        for (int i=1; i<=a;++i) {
+            for (int j = 0; j <= a-i ; ++j) {
+                System.out.print("  ");
+            }
+            for (int k=1; k<=i;k++) {
+                System.out.print(k+" ");
+                }
+            for (int l=i-1; l>=1;l--) {
+                System.out.print(l + " ");
+            }
+            System.out.println();
+        }
+    }
+}
