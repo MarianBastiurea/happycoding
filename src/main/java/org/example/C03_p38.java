18ff79d9f47d9a661f4adaefebf4743f3b4411a5
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-19 17:26:16 +0000

    Check if a number is palindrome

---
+++ src/main/java/org/example/C03_p38.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+class C03_p38 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int reverseNumber = 0;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number to check if it is a palindrome: ");
+        a= sc.nextInt();
+        b=a;
+        for( ;a != 0; a=a/10) {
+            int remainder = a % 10;
+            reverseNumber = reverseNumber * 10 + remainder;
+        }
+        if (b==reverseNumber) {
+            System.out.println("The number " + b + " is a palindrome");
+        }
+        else {
+            System.out.println("The number " + b + " is not a palindrome");
+        }
+    }
+}
