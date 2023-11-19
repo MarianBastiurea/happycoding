2309be5a29f606d57cf4eb7d25bdff8ecfdf05ce
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-19 18:14:49 +0000

    Find LCM of any two numbers

---
+++ src/main/java/org/example/C03_p45.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+ class C03_p45 {
+    public static void main(String[] args) {
+        int a, b;
+        int c = 1;
+        int LCM;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first integer:");
+        a = sc.nextInt();
+        System.out.print("Enter second integer:");
+        b = sc.nextInt();
+        for (int i = 1; i <= a && i <= b; ++i) {
+            if (a % i == 0 && b % i == 0) {
+                c = i;
+            }
+        }
+        LCM = (a * b) / c;
+        System.out.println("The LCM of " + a + " and " + b + " is " + LCM);
+    }
+}
