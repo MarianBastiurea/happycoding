0f0e75e4d20653e4ef723eeb0f34fb90f0d8f8b2
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-22 10:03:27 +0000

    Check if a string is palindrome

---
+++ src/main/java/org/example/Palindrome.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Palindrome {
+    public boolean isPalindrome(char[] string) {
+        boolean result = false;
+        if (string == null) {
+            result = false;
+        } else if (string.length==1) {
+            result = true;
+        } else if(string.length>1) {
+            for (int head=0, tail=string.length-1; head<tail;head++,tail--) {
+            if (string[head]==string[tail]) {
+                result = true;
+            } else {
+                result=false;
+                break;
+            }
+            }
+        }
+
+        return result;
+    }
+}
