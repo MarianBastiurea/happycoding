2012208c50a11a329819c15e8f6e26f5eae02709
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-23 10:14:40 +0000

    Read n array elements
    input number of array's elements, input array's elements and display in reverse order

---
+++ src/main/java/org/example/C5_p02.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+class C05_p02{
+    public static void main(String[] args) {
+        int a,j;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input number of array's element: ");
+        a= sc.nextInt();
+        int[] myArray= new int[a];
+        for(int i=0; i<a;++i) {
+            System.out.print("Input "+i+" element of array: ");
+            myArray[i]= sc.nextInt();
+            System.out.println();
+        }
+        for(j=a-1; j>=0; j--) {
+            System.out.print(myArray[j]+" ");
+        }
+    }
+}
