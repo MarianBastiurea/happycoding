12fb0f5d2aefbbe0a52167331c643fb1d5a1bf06
MarianBastiurea <marian.bastiurea@gmail.com>
2023-11-26 21:43:54 +0000

    Merge two arrays and sort in descending order

---
+++ src/main/java/org/example/C5P07MergeAndSortDescendingTwoArray.java
@@ -0,0 +1,30 @@
+package org.example;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class C5P07MergeAndSortDescendingTwoArray {
+    public int[] MergeAndSort(int [] FirstArray, int [] SecondArray) {
+        int lengthArray= FirstArray.length+SecondArray.length;
+        int [] myArray=new int[lengthArray];
+        int i=0;
+        int j=0;
+        int k=0;
+        while (i< FirstArray.length) {
+            myArray[k]=FirstArray[i];
+            i++;
+            k++;
+        }
+        while (j< SecondArray.length) {
+            myArray[k]=SecondArray[j];
+            j++;
+            k++;
+        }
+        Arrays.sort(myArray);
+        for( int n=0; n<lengthArray/2; n++) {
+            int temp=myArray[n];
+            myArray[n]=myArray[lengthArray-n-1];
+            myArray[lengthArray-n-1]=temp;
+        }
+        return myArray;
+    }
+}
---
+++ src/test/java/org/example/C5P06MergeAndSortDescendingTwoArrayTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class C5P06MergeAndSortDescendingTwoArrayTest {
+    @Test
+    public void MergeAndSortTwoArray() {
+        C5P07MergeAndSortDescendingTwoArray obj= new C5P07MergeAndSortDescendingTwoArray();
+        int [] FirstArray= new int [3];
+        int [] SecondArray = new int[3];
+        FirstArray[0]=1;
+        FirstArray[1]=2;
+        FirstArray[2]=3;
+        SecondArray[0]=1;
+        SecondArray[1]=2;
+        SecondArray[2]=3;
+        int[] result =obj.MergeAndSort(FirstArray,SecondArray);
+        assertArrayEquals(result, new int[] {3,3,2,2,1,1});
+    }
+}
