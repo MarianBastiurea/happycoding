51d324f32c63aa40ba7beeec51e09039682dd96f
MarianBastiurea <marian.bastiurea@gmail.com>
2023-12-04 09:46:13 +0000

    Changes in algorithm

--- src/main/java/org/example/C05P12SortInDescendingOrderElementsOfArray.java
+++ src/main/java/org/example/C05P12SortInDescendingOrderElementsOfArray.java
@@ -3,25 +3,17 @@ package org.example;
 public class C05P12SortInDescendingOrderElementsOfArray {
     public static int[] SortInDescendingOrder(int[] myArray) {
             int[] resultArray = new int[myArray.length];
-            for (int i = 0; i < myArray.length; i++) {
-                int max = 0;
-                for (int j = 0; j < myArray.length; j++) {
-                    max = Math.max(max, myArray[j]);
+        int temp = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = i + 1; j < myArray.length; j++) {
+                if (myArray[i] < myArray[j]) {
+                    temp = myArray[i];
+                    myArray[i] = myArray[j];
+                    myArray[j] = temp;
                 }
-                int x= findIndexOfMax(myArray,max);
-                myArray[x] = 0;
-                resultArray[i] = max;
             }
-            return resultArray;
         }
-        static int findIndexOfMax(int[] myArray,int max){
-            int index;
-            for(index=0;index< myArray.length; index++) {
-                if (myArray[index] == max) {
-                    return index;
-                }
-            }
-            return index;
+        return myArray;
         }
     }
 
