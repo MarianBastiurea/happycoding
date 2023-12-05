51ba9962c6a7e88226568d5bc6376d3b7f55604f
MarianBastiurea <marian.bastiurea@gmail.com>
2023-12-05 13:56:03 +0000

    Changes in algorithm

--- src/main/java/org/example/C05P16FindTheSecondLargestElementInAnArray.java
+++ src/main/java/org/example/C05P16FindTheSecondLargestElementInAnArray.java
@@ -1,17 +1,19 @@
 package org.example;
 
 public class C05P16FindTheSecondLargestElementInAnArray {
-    public int[] FindSecondLargestElementInArray(int[] myArray) {
+    public static int[] FindSecondLargestElementInArray(int[] myArray) {
         int[] resultArray = new int[1];
         int temp;
-        for (int i=0;i<4;i++) {
-            if (myArray[i] > myArray[i + 1]) {
-                temp = myArray[i + 1];
-                myArray[i + 1] = myArray[i];
-                myArray[i] = temp;
+        for (int i=0;i< myArray.length-1;i++) {
+            for (int j = i + 1; j < myArray.length; j++) {
+                if (myArray[i] > myArray[j]) {
+                    temp = myArray[i];
+                    myArray[i] = myArray[j];
+                    myArray[j] = temp;
+                }
             }
         }
-        resultArray[0]=myArray[3];
+        resultArray[0]=myArray[myArray.length-2];
         return resultArray;
     }
 }
--- src/test/java/org/example/C05P16FindTheSecondLargestElementInAnArrayTest.java
+++ src/test/java/org/example/C05P16FindTheSecondLargestElementInAnArrayTest.java
@@ -2,19 +2,26 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.example.C05P16FindTheSecondLargestElementInAnArray.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class C05P16FindTheSecondLargestElementInAnArrayTest {
     @Test
     public void FiveElements(){
-        C05P16FindTheSecondLargestElementInAnArray obj=new C05P16FindTheSecondLargestElementInAnArray();
-        int[] myArray=new int[5];
-        myArray[0]=2;
-        myArray[1]=9;
-        myArray[2]=1;
-        myArray[3]=4;
-        myArray[4]=6;
-        int[] result=obj.FindSecondLargestElementInArray(myArray);
+        int[] myArray={2,9,1,4,6};
+        int[] result=C05P16FindTheSecondLargestElementInAnArray.FindSecondLargestElementInArray(myArray);
         assertArrayEquals(result,new int[] {6});
+    }
+    @Test
+    public void TwoElements(){
+        int[] myArray={100,2};
+        int[] result=C05P16FindTheSecondLargestElementInAnArray.FindSecondLargestElementInArray(myArray);
+        assertArrayEquals(result,new int[] {2});
+    }
+    @Test
+    public void ThreeElements(){
+        int[] myArray={100,9,-2};
+        int[] result=C05P16FindTheSecondLargestElementInAnArray.FindSecondLargestElementInArray(myArray);
+        assertArrayEquals(result,new int[] {9});
     }
 }
