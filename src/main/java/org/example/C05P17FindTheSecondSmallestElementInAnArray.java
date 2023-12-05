190583835f4d522fe0360a49de7f42acb8341a78
MarianBastiurea <marian.bastiurea@gmail.com>
2023-12-05 18:21:07 +0000

    changes in algorithm

--- src/main/java/org/example/C05P17FindTheSecondSmallestElementInAnArray.java
+++ src/main/java/org/example/C05P17FindTheSecondSmallestElementInAnArray.java
@@ -1,14 +1,16 @@
 package org.example;
 
 public class C05P17FindTheSecondSmallestElementInAnArray {
-    public int[] SecondSmallestElementInAnArray(int[] myArray){
+    public static int[] SecondSmallestElementInAnArray(int[] myArray){
         int[] resultArray=new int[1];
         int temp;
-        for (int i=0;i<4;i++) {
-            if (myArray[i] > myArray[i + 1]) {
-                temp = myArray[i + 1];
-                myArray[i + 1] = myArray[i];
-                myArray[i] = temp;
+        for (int i=0;i< myArray.length;i++) {
+            for (int j = i + 1; j < myArray.length; j++) {
+                if (myArray[i] > myArray[j]) {
+                    temp = myArray[i];
+                    myArray[i] = myArray[j];
+                    myArray[j] = temp;
+                }
             }
         }
         resultArray[0]=myArray[1];
--- src/test/java/org/example/C05P17FindTheSecondSmallestElementInAnArrayTest.java
+++ src/test/java/org/example/C05P17FindTheSecondSmallestElementInAnArrayTest.java
@@ -6,14 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class C05P17FindTheSecondSmallestElementInAnArrayTest {
     @Test
     public void FiveElements() {
-        C05P17FindTheSecondSmallestElementInAnArray obj = new C05P17FindTheSecondSmallestElementInAnArray();
-        int[] myArray = new int[5];
-        myArray[0] = 0;
-        myArray[1] = 9;
-        myArray[2] = 4;
-        myArray[3] = 6;
-        myArray[4] = 5;
-        int[] result = obj.SecondSmallestElementInAnArray(myArray);
+        int[] myArray = {0,9,4,6,5};
+        int[] result = C05P17FindTheSecondSmallestElementInAnArray.SecondSmallestElementInAnArray(myArray);
         assertArrayEquals(result, new int[]{4});
+    }
+    @Test
+    public void TwoElements() {
+        int[] myArray = {0,-100};
+        int[] result = C05P17FindTheSecondSmallestElementInAnArray.SecondSmallestElementInAnArray(myArray);
+        assertArrayEquals(result, new int[]{0});
+    }
+    public void SixElements() {
+        int[] myArray = {0,-100,90,1,-50,9};
+        int[] result = C05P17FindTheSecondSmallestElementInAnArray.SecondSmallestElementInAnArray(myArray);
+        assertArrayEquals(result, new int[]{-50});
     }
 }
