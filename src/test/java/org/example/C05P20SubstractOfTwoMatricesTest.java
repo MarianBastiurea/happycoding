d6f6b126b57ee8a5bd1069022994ea0c3d0fc21d
MarianBastiurea <marian.bastiurea@gmail.com>
2023-12-10 10:19:42 +0000

    Add a test with 4x4 matrix.

--- src/test/java/org/example/C05P20SubstractOfTwoMatricesTest.java
+++ src/test/java/org/example/C05P20SubstractOfTwoMatricesTest.java
@@ -36,6 +36,14 @@ public class C05P20SubstractOfTwoMatricesTest {
         int[][] result = C05P20SubstractOfTwoMatrices.SubstractOftwoMatrices(FirstMatrices, SecondMatrices);
         assertArrayEquals(ResultMatrices, result);
     }
+    @Test
+    public void Substract4x4Matrices() {
+        int[][] FirstMatrices ={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        int[][] SecondMatrices ={{16,15,14,13},{12,11,10,9},{8,7,6,5},{4,3,2,1}};
+        int[][] ResultMatrices ={{-15,-13,-11,-9},{-7,-5,-3,-1},{1,3,5,7},{9,11,13,15}};
+        int[][] result = C05P20SubstractOfTwoMatrices.SubstractOftwoMatrices(FirstMatrices, SecondMatrices);
+        assertArrayEquals(ResultMatrices, result);
+    }
 }
 
 
