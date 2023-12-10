10afa21b14a64019a658723e9b0dd6a731688672
MarianBastiurea <marian.bastiurea@gmail.com>
2023-12-10 17:50:03 +0000

    Finish algorithm
    I forgot to write final condition of test

--- src/test/java/org/example/C05P26_Change_Elements_In_Lower_Triangular_Of_A_Matrix_Test.java
+++ src/test/java/org/example/C05P26_Change_Elements_In_Lower_Triangular_Of_A_Matrix_Test.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class C05P26_Change_Elements_In_Lower_Triangular_Of_A_Matrix_Test {
     @Test
@@ -9,12 +10,14 @@ public class C05P26_Change_Elements_In_Lower_Triangular_Of_A_Matrix_Test {
         int[][] myMatrix={{1,2,3},{4,5,6},{7,8,9}};
         int[][] resultMatrix={{1,2,3},{0,5,6},{0,0,9}};
         int[][] result=C05P26_Change_Elements_In_Lower_Triangular_Of_A_Matrix.Change_Elements(myMatrix);
+        assertArrayEquals(result,resultMatrix);
     }
     @Test
     public  void FourElementsMatrix(){
         //Changes elements with 0
         int[][] myMatrix={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        int[][] resultMatrix={{1,2,3,4},{0,5,6,7},{0,0,11,12},{0,0,0,16}};
+        int[][] resultMatrix={{1,2,3,4},{0,6,7,8},{0,0,11,12},{0,0,0,16}};
         int[][] result=C05P26_Change_Elements_In_Lower_Triangular_Of_A_Matrix.Change_Elements(myMatrix);
+        assertArrayEquals(result,resultMatrix);
     }
 }
