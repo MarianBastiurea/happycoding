e93e8bcf45e59157651704dbb7ef75bc631541ad
MarianBastiurea <marian.bastiurea@gmail.com>
2023-12-10 16:19:38 +0000

    Sum of elements from right diagonal in a matrix

---
+++ src/main/java/org/example/C05P23_Sum_Of_Right_Diagonal_In_Matrix.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class C05P23_Sum_Of_Right_Diagonal_In_Matrix {
+    public static int[] Sum_Of_Diagonal(int[][] myMatrix){
+        int[] sum={0};
+        for (int i=0; i< myMatrix[0].length;i++){
+            for(int j=0;j<myMatrix.length;j++){
+                if (i==j){
+                    sum[0]=sum[0]+myMatrix[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+}
