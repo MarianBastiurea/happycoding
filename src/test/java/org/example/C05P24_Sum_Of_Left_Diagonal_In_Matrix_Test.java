ddd48f4388ec8c10022a3da7bfbdbda334b66b04
MarianBastiurea <marian.bastiurea@gmail.com>
2023-12-10 16:20:03 +0000

    Sum of elements from left diagonal in a matrix

---
+++ src/test/java/org/example/C05P24_Sum_Of_Left_Diagonal_In_Matrix_Test.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class C05P24_Sum_Of_Left_Diagonal_In_Matrix_Test {
+    @Test
+    public void Two_Rows_And_Two_Columns() {
+        int[][] myMatrix = {{1, 2}, {3, 4}};
+        int[] sum ={5};
+        int[] result = C05P24_Sum_Of_Left_Diagonal_In_Matrix.Sum_Of_Diagonal(myMatrix);
+        assertArrayEquals(result, sum);
+    }
+    @Test
+    public void A4x4Matrix() {
+        int[][] myMatrix = {{1,2,3,4},{0,1,3,0},{0,0,10,0},{0,0,0,10}};
+        int[] sum ={7};
+        int[] result = C05P24_Sum_Of_Left_Diagonal_In_Matrix.Sum_Of_Diagonal(myMatrix);
+        assertArrayEquals(result, sum);
+    }
+    @Test
+    public void EmptyMatrix() {
+        int[][] myMatrix = {{}};
+        int[] sum ={0};
+        int[] result = C05P24_Sum_Of_Left_Diagonal_In_Matrix.Sum_Of_Diagonal(myMatrix);
+        assertArrayEquals(result, sum);
+    }
+}
