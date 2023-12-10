47011ffe6cc38cdb4e645254a2e0443ee8d98281
MarianBastiurea <marian.bastiurea@gmail.com>
2023-12-10 16:30:58 +0000

    Change in algorithm
    In first solution I use "sum" as an array. In second one, I use "sum" as integer.

--- src/main/java/org/example/C05P24_Sum_Of_Left_Diagonal_In_Matrix.java
+++ src/main/java/org/example/C05P24_Sum_Of_Left_Diagonal_In_Matrix.java
@@ -1,13 +1,13 @@
 package org.example;
 
 public class C05P24_Sum_Of_Left_Diagonal_In_Matrix {
-    public static int[] Sum_Of_Diagonal(int[][] myMatrix){
-        int[] sum={0};
+    public static int Sum_Of_Diagonal(int[][] myMatrix){
+        int sum=0;
         int decrement= myMatrix.length-1;
         for (int i=0;i<myMatrix[0].length;i++){
             for(int j=myMatrix.length-1;j>=0;j--){
                 if(j==decrement) {
-                    sum[0] = sum[0] + myMatrix[i][j];
+                    sum = sum + myMatrix[i][j];
                 }
             }
             decrement=decrement-1;
