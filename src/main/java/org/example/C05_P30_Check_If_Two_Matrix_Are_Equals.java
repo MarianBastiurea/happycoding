9c8a64a792f4f6214657178263b1c3a40db2a6cf
MarianBastiurea <marian.bastiurea@gmail.com>
2023-12-10 19:15:43 +0000

    Check if two matrix are equal

---
+++ src/main/java/org/example/C05_P30_Check_If_Two_Matrix_Are_Equals.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class C05_P30_Check_If_Two_Matrix_Are_Equals {
+    public static boolean Check_Two_Matrix(int[][] FirstMatrix, int[][] SecondMatrix){
+        for(int i=0;i< FirstMatrix[0].length; i++){
+            for(int j=0; j< FirstMatrix.length; j++){
+                if(FirstMatrix[i][j]!=SecondMatrix[i][j]){
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/org/example/C05_P30_Check_If_Two_Matrix_Are_Equals_Test.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class C05_P30_Check_If_Two_Matrix_Are_Equals_Test {
+    @Test
+    public void Three_Element_Matrix(){
+        int[][] FirstMatrix={{1,2,3},{4,5,6},{7,8,9}};
+        int[][] SecondMatrix={{1,2,3},{4,5,6},{7,8,9}};
+        boolean result=C05_P30_Check_If_Two_Matrix_Are_Equals.Check_Two_Matrix(FirstMatrix,SecondMatrix);
+        assertTrue(result);
+    }
+}
