cbd1847475a31482fbec86ab818fb7be570222a5
MarianBastiurea <marian.bastiurea@gmail.com>
2023-12-25 19:40:46 +0000

    Add comments to algorithm

--- src/main/java/org/example/Find_The_Longest_Substring_Between_Two_Same_Characters.java
+++ src/main/java/org/example/Find_The_Longest_Substring_Between_Two_Same_Characters.java
@@ -2,8 +2,13 @@ package org.example;
 
 public class Find_The_Longest_Substring_Between_Two_Same_Characters {
     public static String FindLongestSubstring(String myString) {
+        // define a matrix where we store repeating element and distance between them
         int[][] LongestSubstring = new int[myString.length()][2];
+
+        // count position of LongestSubstring where we store data.
         int k = 0;
+
+        // create matrix where will store repeating element and distance between them
         for (int i = 0; i < myString.length(); i++) {
             for (int j = i + 1; j < myString.length(); j++) {
                 if (myString.charAt(i) == myString.charAt(j)) {
@@ -13,8 +18,13 @@ public class Find_The_Longest_Substring_Between_Two_Same_Characters {
                 }
             }
         }
+
+        // define two temporary values to store first position of repeating element and
+        // distance to second repeating  element
         int tempI = 0, tempJ = 0;
 
+        // Find the biggest distance between two same elements
+        // will be the last row in matrix
         for (int i = 0; i < k; i++) {
             for (int j = i + 1; j < k; j++) {
                 if (LongestSubstring[i][1] > LongestSubstring[j][1]) {
@@ -29,6 +39,7 @@ public class Find_The_Longest_Substring_Between_Two_Same_Characters {
                 }
             }
         }
+        // define two values start and end for longest substring
         int start = LongestSubstring[k - 1][0];
         int end = LongestSubstring[k - 1][1];
         String resultString = myString.substring(start, end);
