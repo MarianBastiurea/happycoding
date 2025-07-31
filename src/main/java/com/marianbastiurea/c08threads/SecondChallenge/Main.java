3d767d8d8023ccfa95094b1b8ba17bb4bef9108b
MarianBastiurea <marian.bastiurea@gmail.com>
2025-07-31 19:01:50 +0100

    use ExecutorService to create Main and ShoeWarehouse

---
+++ src/main/java/com/marianbastiurea/c08threads/SecondChallenge/Main.java
@@ -0,0 +1,57 @@
+package com.marianbastiurea.c08threads.SecondChallenge;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+record Order(long orderId, String item, int qty) {
+}
+
+public class Main {
+
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+        ShoeWarehouse warehouse = new ShoeWarehouse();
+        ExecutorService orderingService = Executors.newCachedThreadPool();
+        Callable<Order> orderingTask = () -> {
+            {
+                Order newOrder = generateOrder();
+                try {
+                    Thread.sleep(random.nextInt(500, 5000));
+                    warehouse.receiveOrder(newOrder);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException();
+                }
+                return newOrder;
+
+            }
+        };
+
+        List<Callable<Order>> task = Collections.nCopies(15, orderingTask);
+        try {
+            orderingService.invokeAll(task);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        orderingService.shutdown();
+        try {
+            orderingService.awaitTermination(6, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        warehouse.shutDown();
+    }
+
+    private static Order generateOrder() {
+        return new Order(
+                random.nextLong(1000000, 9999999),
+                ShoeWarehouse.PRODUCT_LIST[random.nextInt(0, 5)],
+                random.nextInt(1, 4));
+    }
+}
+
